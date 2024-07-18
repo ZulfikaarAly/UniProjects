3c2e88be1fa47e9aed7fb66a824963542d384899
ZulfikaarAly <za241967@gmail.com>
2024-07-18 16:08:29 +0500

    Add files via upload

---
+++ Chess2D/MainGame.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <stdlib.h>
+#include <SFML\Graphics.hpp>
+#include "Pieces.h"
+using namespace sf;
+using namespace std;
+
+int main() {
+    ChessBoard qGame;
+    qGame.Start();
+    return 0;
+}
